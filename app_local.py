5f6172ac97936e2b4e0d884ae16c4ec62e8e1fd6
seannnnnn1017 <113087774+seannnnnn1017@users.noreply.github.com>
2023-12-01 16:01:04 +0800

    upload

--- app_local.py
+++ app_local.py
@@ -4,3 +4,16 @@ import os
 from pymongo import MongoClient
 from datetime import timedelta
 import requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)

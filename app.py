8a0e06ae9aee593fd40871c49dbca39c9c1121fb
seannnnnn1017 <113087774+seannnnnn1017@users.noreply.github.com>
2023-12-02 14:26:17 +0800

    upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, url_for, redirect,session,jsonify,abort,redirect
+from datetime import datetime, timedelta
+import os
+from pymongo import MongoClient
+from datetime import timedelta
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
